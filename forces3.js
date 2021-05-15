c03b18aff42f8aaa410ca0af22d9dba8e95f5dcf
iyoman <42587833+iyoman@users.noreply.github.com>
2021-05-15 17:32:58 -0400

    Update forces3.js

--- forces3.js
+++ forces3.js
@@ -5,17 +5,7 @@ function setup() {
   colorMode(HSB, 100);
   debug = 0;
 
-  b1 = new Bob({
-    x: 200,
-    y: 100,
-    vx: 0,
-    size: 30,
-    link: [],
-    ay: 0.1,
-    mass: 1,
-  });
-  
-  new Wall(0, windowHeight - 10,windowWidth,windowHeight-10)
+  new Wall(0, windowHeight - 10, windowWidth, windowHeight - 10);
 
   mainlist.forEach(function (e) {
     e.initialize();
@@ -80,7 +70,6 @@ class Bob {
       this.f.add(this.vec.add(df));
       drawArrow(this.l, this.vec, "blue", 20);
     }
-    
   }
 
   finalupdate() {
@@ -88,7 +77,7 @@ class Bob {
     this.l.add(this.v.copy().mult(dt));
     collide(this);
     collide(this);
-    this.nocol = 0
+    this.nocol = 0;
     if (this.y > 1000) {
       let index = mainlist.indexOf(this);
       mainlist.splice(index, 1);
@@ -97,15 +86,15 @@ class Bob {
 
   render() {
     push();
-    
+
     fill(this.color[0], this.color[1], this.color[2]);
     ellipse(this.l.x, this.l.y, this.size);
     drawArrow(this.l, this.f, "green", 50);
     drawArrow(this.l, this.v, "red", 5);
-    for (var i = 0; i< this.links.length; i++) {
-      drawArrow(this.l, this.links[i][0].l.copy().sub(this.l), 'orange', 0.5)
+    for (var i = 0; i < this.links.length; i++) {
+      drawArrow(this.l, this.links[i][0].l.copy().sub(this.l), "orange", 0.5);
     }
-    
+
     pop();
   }
 
@@ -113,7 +102,7 @@ class Bob {
     for (var i in this.links) {
       if (this.links[i][2] == 1) {
         this.links[i][0].links.push([this, this.links[i][1]]);
-      } 
+      }
     }
   }
 }
@@ -143,7 +132,7 @@ class Wall {
   update() {}
   finalupdate() {}
   initialize() {}
-  
+
   render() {
     push();
     stroke(60, 100, 70);
@@ -153,7 +142,8 @@ class Wall {
   }
 }
 
-function draw() {/////////////////////draw
+function draw() {
+  /////////////////////draw
   background(255);
   frameRate(60);
   mVelocity();
@@ -165,72 +155,73 @@ function draw() {/////////////////////draw
     e.render();
   });
   paused();
-  textwall(windowWidth - 150, 20, ["mv", mAx], ["objects",mainlist.length]);
-  if (keyIsDown(UP_ARROW)) {
-    dotslist[0].l.y += 20
-    }
-  if (keyIsDown(DOWN_ARROW)) {
-    dotslist[0].l.y -= 20
-    }
-  if (keyIsDown(RIGHT_ARROW)) {
-    dotslist[0].l.x += 20
-    }
-  if (keyIsDown(LEFT_ARROW)) {
-    dotslist[0].l.x -= 20
-    }
-  if (keyIsDown(65)) {
-    dotslist[0].v.y = 0
-    dotslist[0].v.x = 0
-    }
-  
-  if (mouseIsPressed && keyIsDown(16)) {
-      if (down == 0) {
-        x1 = mouseX;
-        y1 = mouseY;
-      }
-      down = 1;
-      push();
-      fill(40);
-      stroke(67, 20, 100, 50);
-      strokeWeight(50);
-      line(x1, y1, mouseX, mouseY);
-      stroke(40,20,100,80)
-      strokeWeight(5)
-      line(x1,y1,mouseX,mouseY)
-      pop();
-    } else if (down == 1) {
-      new Wall(x1, y1, mouseX, mouseY);
-      down = 0;
-    }
-  
-}//draw///////////////////////////////end of draw loop
+  instructions(
+    20, 
+    120,
+    ["Place a dot", "click"],
+    ["Link two dots", "click on one dot, then another"],
+    ["Pause simulation", "space"],
+    ["Draw wall", "shift + click"],
+    ["Close instructions", "i"]
+  );
+  textwall(
+    windowWidth - 150,
+    20,
+    ["mousevelocity", mAx],
+    ["total objects", mainlist.length],
+    ["walls", wallist.length],
+    ["points", dotslist.length]
+  );
 
-  var dot1 = 0
-  var dot2 = 0
-  function mouseClicked() {
-    for (var i = 0; i<dotslist.length; i++) {
-    dot = dotslist[i]
-    if (dot.l.copy().sub(mouseX,mouseY).magSq() < dot.size*dot.size/2) {
+  if (mouseIsPressed && keyIsDown(16)) {
+    if (down == 0) {
+      x1 = mouseX;
+      y1 = mouseY;
+    }
+    down = 1;
+    push();
+    fill(40);
+    stroke(67, 20, 100, 50);
+    strokeWeight(50);
+    line(x1, y1, mouseX, mouseY);
+    stroke(40, 20, 100, 80);
+    strokeWeight(5);
+    line(x1, y1, mouseX, mouseY);
+    pop();
+  } else if (down == 1) {
+    new Wall(x1, y1, mouseX, mouseY);
+    down = 0;
+  }
+} //draw///////////////////////////////end of draw loop
+
+var dot1 = 0;
+var dot2 = 0;
+function mouseClicked() {
+  for (var i = 0; i < dotslist.length; i++) {
+    dot = dotslist[i];
+    if (dot.l.copy().sub(mouseX, mouseY).magSq() < (dot.size * dot.size) / 2) {
       if (dot1 == 0) {
-        dot1 = dot
+        dot1 = dot;
       } else {
-        dot2 = dot
-      }
+        dot2 = dot;
       }
     }
-    if (dot1 != 0 && dot2 != 0) {
-      let dist = dot1.l.copy().sub(mouseX,mouseY).mag()
-      dot1.links.push([dot2,dist])
-      dot2.links.push([dot1,dist])
-      dot1 = 0
-      dot2 = 0
-    } else if (dot1 == 0 && dot2 == 0 && down == 0) {
-      new Bob({x:mouseX,y:mouseY,size:40})
-    }
-    
-    
-  }//mouseClicked
-  
+  }
+  if (dot1 != 0 && dot2 != 0) {
+    let dist = dot1.l.copy().sub(mouseX, mouseY).mag();
+    dot1.links.push([dot2, dist]);
+    dot2.links.push([dot1, dist]);
+    dot1 = 0;
+    dot2 = 0;
+  } else if (dot1 == 0 && dot2 == 0 && down == 0) {
+    new Bob({
+      x: mouseX,
+      y: mouseY,
+      size: 40,
+      color: [Math.random() * 100, 100, 100],
+    });
+  }
+} //mouseClicked
 
 function keyTyped() {
   if (keyCode === 32 && dt == 1) {
@@ -290,8 +281,20 @@ function paused() {
   text(pause, 20, 65);
 }
 
+function instructions(x, y, ...args) {
+  push();
+  textSize(window.innerHeight / 20);
+  if (key == 'i') {
+    fill(100,100,100,0)
+  }
+  for (var a in args) {
+    text(args[a][0] + ": " + args[a][1], x, y + a * 50);
+  }
+  pop();
+}
+
 function drawArrow(base, vec, myColor, scal) {
-  if (debug || myColor == 'orange') {
+  if (debug || myColor == "orange") {
     let scl = scal;
     push();
     strokeWeight(1);
@@ -319,7 +322,7 @@ function collide(obj) {
   obj.px = obj.pl.x;
   obj.x = obj.l.x;
   obj.y = obj.l.y;
-  let clist = []
+  let clist = [];
   for (var i = 0; i < wallist.length; i++) {
     let wall = wallist[i];
     if (wall !== obj.nocol) {
@@ -345,20 +348,22 @@ function collide(obj) {
           btwn(wall.ax, wall.bx, obj.cx) &&
           btwn(wall.ay, wall.by, obj.cy)
         ) {
-          clist.push({x:obj.cx,y:obj.cy,w:wall})
+          clist.push({ x: obj.cx, y: obj.cy, w: wall });
         }
       }
     }
   }
-  let wally = 0
-  if (clist.length > 0) { wally = mindist(obj.l, clist);}
-  if (wally != 0) { 
-  obj.l = obj.pl.copy()
-  obj.pl = obj.l.copy();
-  obj.v.reflect(wally.vec.copy().rotate(HALF_PI));
-  obj.l.add(obj.v);
-  obj.v.mult(wally.damping);
-  obj.nocol = wally;
+  let wally = 0;
+  if (clist.length > 0) {
+    wally = mindist(obj.l, clist);
+  }
+  if (wally != 0) {
+    obj.l = obj.pl.copy();
+    obj.pl = obj.l.copy();
+    obj.v.reflect(wally.vec.copy().rotate(HALF_PI));
+    obj.l.add(obj.v);
+    obj.v.mult(wally.damping);
+    obj.nocol = wally;
   }
 }
 
